77db7a938e4481bb02c58a8fd0724848f35a57bd
Adeeb Rahman <44913977+adeebrahman@users.noreply.github.com>
2019-10-17 22:32:29 -0400

    Rendering App

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import App from './App.js';
 import SessionPage from './containers/SessionPage';
 
-ReactDOM.render(<SessionPage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
