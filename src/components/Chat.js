649d3916dcfe9e7b16e7f60eea60a89fefac6d6d
Daniel Lu <daniellu6688@hotmail.com>
2019-11-02 15:15:33 -0400

    allows user to enter chat messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,39 +3,75 @@ import '../css/components/Chat.css';
 import ReceivedMessage from './ReceivedMessage';
 import SentMessage from './SentMessage';
 
-class Chat extends React.Component {
+class Chat extends React.Component { 
+
+	state = {
+		message: "",
+		messages: []
+	}
+
+	handleNewMessage = (event) =>{
+		const target = event.target; 
+		const value = target.value; 
+
+		this.setState({
+			message: value
+		})
+	}
+
+	addMessage = () => {
+		const messageList = this.state.messages; 
+		const message = {
+			content: this.state.message,
+			type: "sent-message", 
+			sender: "You"
+		}
+		messageList.push(message); 
+		const message2 = {
+			content: "Generic Response", 
+			type: "received-message", 
+			sender: "UserXYZ"
+		}
+		messageList.push(message2); 
+		console.log(messageList); 
+		this.setState({
+			messages: messageList,
+		})
+	}
+
 	render() {
 		return (
 			<div>
 				<h3 className="chat-header"> Chat with UserXYZ </h3>
+				<div dangerouslySetInnerHTML={{__html: this.element}}></div>
 				<div className="chat-box">
-					<div className="received-message">
-						<ReceivedMessage message="This is a message" />
-					</div>
-					<div className="received-message">
-						<ReceivedMessage message="This is a slightly longer message to show resizing" />
-					</div>
-					<div className="received-message">
-						<ReceivedMessage message="This is a very very very very very very very very long message to show the newline break" />
-					</div>
-					<div className="sent-message">
-						<ReceivedMessage message="This is a very very very very very very very very long message to show the newline break AND the user response!" />
-					</div>
-					<div className="received-message">
-						<ReceivedMessage message="This is a message" />
-					</div>
-					<div className="received-message">
-						<ReceivedMessage message="This is a message" />
-					</div>
-					<div className="received-message">
-						<ReceivedMessage message="This is a message" />
-					</div>
+					{ this.state.messages.map((message) => {
+						if (message.type == "received-message"){
+							return(
+								<div className="received-message">
+									<ReceivedMessage message={message.content} />
+								</div>
+							)
+						}
+						else if (message.type == "sent-message"){
+							return(
+								<div className="sent-message">
+									<SentMessage message={message.content} />
+								</div>
+							)
+						}
+					})
+					}
 				</div>
 
 				<React.Fragment>
 					<form className="message-input-field">
-						<input className="chat-entry" type="text" placeholder="Enter your message here" />
-						<input type="submit" value="Send" />
+						<input  className="chat-entry" 
+								type="text" 
+								placeholder="Enter your message here" 
+								value = {this.state.message}
+								onChange = {this.handleNewMessage}/>
+						<input type="submit" value="Send" onClick = {this.addMessage}/>
 					</form>
 				</React.Fragment>
 
--- src/components/SentMessage.js
+++ src/components/SentMessage.js
@@ -5,6 +5,7 @@ class SentMessage extends React.Component{
     render(){
         return(
             <div>
+                <p class = "speaker"> You </p>
                 <h3 class = "self-message">{this.props.message} </h3>
             </div>
         );
