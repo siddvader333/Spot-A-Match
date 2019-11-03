f3629a708a251fef303306a94b57e67b8baf1055
Daniel Lu <daniellu6688@hotmail.com>
2019-11-02 20:09:14 -0400

    made next song button dynamic

--- src/containers/SessionPage.js
+++ src/containers/SessionPage.js
@@ -5,10 +5,62 @@ import AddSongsBtnSession from '../components/AddSongsBtnSession';
 import UpNextSongList from '../components/UpNextSongList';
 import Chat from '../components/Chat';
 import GroupChat from '../components/GroupChat';
+import CurrentlyPlaying from '../components/CurrentlyPlaying';
 
 class SessionPage extends React.Component {
+	
+	constructor(props){
+		super(props);
+		const song1 = {
+			title: "Gang Up", 
+			artist: "Young Thug, 2 Chainz, Wiz Khalifa, Pnb Rock"
+		}
+		const song2 = {
+			title: "Loaded Gun", 
+			artist: "6lack"
+		}
+		const song3 = {
+			title: "3005", 
+			artist: "Childish Gambino"
+		}
+		const song4 = {
+			title: "Wesley's Theory", 
+			artist: "Kendrick Lamar"
+		}
+		const song5 = {
+			title: "POWER", 
+			artist: "Kanye West"
+		} 
+		const songList = []; 
+		songList.push(song2);
+		songList.push(song3); 
+		songList.push(song4); 
+		songList.push(song5);
+		this.state = {
+			currentPlaying: song1, 
+			currentSongList: songList
+		}
+		this.nextSong = this.nextSong.bind(this); 
+	}
+
+	state = {
+		currentPlaying: "",
+		currentSongList: []
+	}
+
+	nextSong = (e) => {
+		e.preventDefault(); 
+		let currentSong = this.state.currentPlaying;
+		const currentList = this.state.currentSongList; 
+		if (currentList.length > 0)
+			currentSong = currentList.shift(); 
+		this.setState({
+			currentPlaying: currentSong, 
+			currentSongList: currentList
+		})
+	}
+	
 	render() {
-		console.log(window.location.pathname);
 		return (
 			<div className="session-page">
 				<br />
@@ -16,13 +68,17 @@ class SessionPage extends React.Component {
 				<div className="row">
 					<div className="song-queue col-md">
 						<h3 className="up-next-session">Up Next:</h3>
-						<UpNextSongList />
-						<AddSongsBtnSession displayText="+ Add Song" />
+						<UpNextSongList songList = {this.state.currentSongList}/>
+						<AddSongsBtnSession />
 					</div>
 
 					<div className="chat col-md">
 						{window.location.pathname == '/dashboard/session' ? <Chat /> : <GroupChat />}
 					</div>
+
+				</div>
+				<div className="row">
+					<CurrentlyPlaying getnextsong = {this.nextSong} songList = {this.state}/>
 				</div>
 			</div>
 		);
