223b568a9b2383eaa92d5c898ee8fbb4b44a21cb
Siddharth Vijay <vijaysid@Siddharths-Air.home>
2019-11-03 19:23:25 -0500

    Added functionality for adding songs from side-menu. Have to add functionality for not being able to add song when joined a room.

--- src/components/MenuSearchResult.js
+++ src/components/MenuSearchResult.js
@@ -2,7 +2,13 @@ import React from 'react';
 import '../css/components/MenuSearchResult.css';
 const MenuSearchResult = (props) => {
 	return (
-		<div onClick={props.onClick} className="menu-search-result">
+		<div
+			onClick={() => {
+				console.log('search result clicked!');
+				props.onClick(props.song);
+			}}
+			className="menu-search-result"
+		>
 			<p className="search-item-song">{props.song.songName}</p>
 			<p className="search-item-title">{props.song.artist}</p>
 		</div>
--- src/containers/HostRoomContainer.js
+++ src/containers/HostRoomContainer.js
@@ -8,64 +8,77 @@ import CurrentlyPlaying from '../components/CurrentlyPlaying';
 import { fakeSearchResults, suggestedSongs } from '../util/Data.js';
 
 class HostRoomContainer extends React.Component {
-	
-	constructor(props){
+	constructor(props) {
 		super(props);
 
 		//NOTE: WE HARDCODED THESE SONGS INTO OUR APPLICATION
 		//IN OUR REAL APPLICATION, THIS WOULD BE DONE THROUGH THE SPOTIFY API
-		const songList = Array.from(fakeSearchResults)
-		const currentSong = songList.shift(); 
+		const songList = Array.from(fakeSearchResults);
+		const currentSong = songList.shift();
 
 		const suggestedList = Array.from(suggestedSongs);
 
 		this.state = {
-			currentPlaying: currentSong, 
+			currentPlaying: currentSong,
 			currentSongList: songList,
 			suggestedList: suggestedList
-		}
+		};
 		this.nextSong = this.nextSong.bind(this);
 		this.acceptSong = this.acceptSong.bind(this);
 		this.rejectSong = this.rejectSong.bind(this);
 	}
 
 	state = {
-		currentPlaying: "",
+		currentPlaying: '',
 		currentSongList: []
-	}
+	};
 
 	nextSong = (e) => {
-		e.preventDefault(); 
+		e.preventDefault();
 		let currentSong = this.state.currentPlaying;
-		const currentList = this.state.currentSongList; 
-		if (currentList.length > 0)
-			currentSong = currentList.shift(); 
+		const currentList = this.state.currentSongList;
+		if (currentList.length > 0) currentSong = currentList.shift();
 		this.setState({
-			currentPlaying: currentSong, 
+			currentPlaying: currentSong,
 			currentSongList: currentList
-		})
-	}
+		});
+	};
 
-	acceptSong(song){
+	acceptSong(song) {
 		//remove from suggestedList
-		const newSuggestedList =  this.state.suggestedList.filter(			//returns a new list with everything except the song			
-			(item) =>{
-				return( item.songName !== song.songName);
-			}		
+		const newSuggestedList = this.state.suggestedList.filter(
+			//returns a new list with everything except the song
+			(item) => {
+				return item.songName !== song.songName;
+			}
 		);
-		this.setState({suggestedList: newSuggestedList});
+		this.setState({ suggestedList: newSuggestedList });
 		//add
-		const newSongList = this.state.currentSongList.push({songName: song.songName,artist: song.songArtist});
+		const newSongList = this.state.currentSongList.push({ songName: song.songName, artist: song.songArtist });
 	}
 
-	rejectSong = (song)=> {
-		const newSuggestedList = this.state.suggestedList.filter(
-			(item) => {return (item.songName !== song.songName);}
-		);
-		this.setState({suggestedList: newSuggestedList});
-		
+	rejectSong = (song) => {
+		const newSuggestedList = this.state.suggestedList.filter((item) => {
+			return item.songName !== song.songName;
+		});
+		this.setState({ suggestedList: newSuggestedList });
+	};
+
+	static getDerivedStateFromProps(nextProps, prevState) {
+		if (nextProps.songToAdd == {}) {
+			console.log('empty props');
+			return;
+		}
+		if (nextProps.songToAdd.songName !== '') {
+			console.log('new song that is not empty');
+			const newList = prevState.currentSongList;
+			newList.push(nextProps.songToAdd);
+			prevState.currentSongList = newList;
+			nextProps.stopSending();
+			return prevState;
+		}
 	}
-	
+
 	render() {
 		return (
 			<div className="session-page">
@@ -74,7 +87,7 @@ class HostRoomContainer extends React.Component {
 				<div className="row">
 					<div className="song-queue col-md">
 						<h3 className="up-next-session">Up Next:</h3>
-						<UpNextSongList songList = {this.state.currentSongList}/>
+						<UpNextSongList songList={this.state.currentSongList} />
 						<AddSongsBtnSession displayText="+ Add Song" />
 					</div>
 
@@ -82,26 +95,24 @@ class HostRoomContainer extends React.Component {
 						<GroupChat />
 					</div>
 				</div>
-				<br/>
-				<br/>
-				<br/>
+				<br />
+				<br />
+				<br />
 
 				<div className="row">
-					
 					{/*placeholder*/}
 					<div className="col-md">
-						<h3 className>USER LIST TBA</h3>											
+						<h3 className>USER LIST TBA</h3>
 					</div>
 
 					<div className="suggested-queue col-md">
 						<h3 className="suggested-songs">Suggested Songs</h3>
-						<SongSuggestionList 
-							suggestedList={this.state.suggestedList} 
-							acceptSong={this.acceptSong} 
-							rejectSong = {this.rejectSong}
-						/>						
+						<SongSuggestionList
+							suggestedList={this.state.suggestedList}
+							acceptSong={this.acceptSong}
+							rejectSong={this.rejectSong}
+						/>
 					</div>
-
 				</div>
 
 				{/*<div className="row">
