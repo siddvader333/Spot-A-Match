fab6f8d0c966e528041d985d0225c4009299f571
Daniel Lu <daniellu6688@hotmail.com>
2019-11-03 19:02:36 -0500

    Profile Container

---
+++ src/containers/ProfileContainer.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import SideMenu from '../components/SideMenu';
+import { Route, Switch } from 'react-router-dom';
+import DashboardContentHeader from '../components/DashboardContentHeader';
+import DashboardFlashMessage from '../components/DashboardFlashMessage';
+import DashboardOptions from '../components/DashboardOptions';
+import Navbar from '../components/Navbar';
+import authFunctions from '../util/Auth';
+import history from '../util/History';
+import '../css/containers/ProfileContainer.css'
+
+class ProfileContainer extends React.Component {
+
+    constructor(props) {
+		super(props);
+		this.state = {
+			isOpen: false,
+			currentPlaying: {}
+		};
+
+		this.openMenu = this.openMenu.bind(this);
+		this.closeMenu = this.closeMenu.bind(this);
+	}
+
+    openMenu() {
+		console.log('here');
+		this.setState({ isOpen: true });
+	}
+
+	closeMenu() {
+		this.setState({ isOpen: false });
+	}
+
+	playSong(song) {
+		this.setState({ currentSongPlaying: [ song ] });
+	}
+
+    render(){
+        let userType = ""; 
+        if (authFunctions.isPremium())
+            userType = <div className = "profile-stat">Premium User</div>; 
+        else
+            userType = <div className = "profile-stat">Non-Premium User</div>; 
+        return(
+            <div>
+                <SideMenu closeMenu={this.closeMenu} isOpen={this.state.isOpen} />
+				<Navbar openMenu={this.openMenu} />
+                <div id = "profile-picture"></div>
+                <div id = "profile-info">
+                    <div className = "profile-stat">Name: UserXYZ</div>
+                    {userType}
+                    <div className = "profile-stat">Rooms Joined: 42</div>
+                    <div className = "profile-stat">Rooms Hosted: 0</div>
+                    <div className = "profile-stat">Sessions Joined: 37</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ProfileContainer;
