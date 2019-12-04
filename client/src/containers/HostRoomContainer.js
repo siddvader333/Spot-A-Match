16249a9699cd83ceeb60a0d1d3f63d48d34511e8
Md Sadat Ar-Raafe <sadatarraafe@gmail.com>
2019-12-03 21:56:09 -0500

    A room host can now play songs.

--- client/src/containers/HostRoomContainer.js
+++ client/src/containers/HostRoomContainer.js
@@ -21,26 +21,64 @@ class HostRoomContainer extends React.Component {
 		const suggestedList = Array.from(suggestedSongs);
 
 		this.state = {
-			currentPlaying: currentSong,
-			currentSongList: songList,
-			suggestedList: suggestedList
+			currentPlaying: '',//currentSong,
+			currentSongList: [],//songList,
+			suggestedList: suggestedList,
+			deviceID: "",
+	 		player_Spotify: {}
 		};
 		this.nextSong = this.nextSong.bind(this);
 		this.acceptSong = this.acceptSong.bind(this);
 		this.rejectSong = this.rejectSong.bind(this);
+		this.pauseSong= this.pauseSong.bind(this);
 
 		var socket = io.connect('https://mighty-refuge-58998.herokuapp.com/host-session');
 		socket.on('connect', function(data) {
 			console.log('host-session socket connected');
 		});
+	
 	}
 
-	state = {
-		currentPlaying: '',
-		currentSongList: []
-	};
+	// state = {
+	// 	currentPlaying: '',
+	// 	currentSongList: [],
+	// 	deviceID: "",
+	// 	player_Spotify: {}
+	// };
 
-	nextSong = (e) => {
+	async componentDidMount(){
+		const response = await fetch('/profile', {
+			method: 'GET',
+			headers: { 'Content-Type': 'applications/json' }
+		});
+		const responseJSON = await response.json();
+		const token = responseJSON.currentAccessToken;
+
+		var player = new window.Spotify.Player({
+			name: "Spot-A-Match Player",
+			getOAuthToken: (callback) => {
+				callback(token)
+			},
+			volume: 0.5
+		})
+		// console.log("the player");
+		// console.log(player);
+		console.log(token);
+
+		player.connect().then(success => {
+			if (success) {
+			  console.log('The Web Playback SDK successfully connected to Spotify!');
+			}
+		})
+		player.addListener('ready', ({ device_id }) => {
+			console.log('The Web Playback SDK is ready to play music!');
+			console.log('Device ID', device_id);
+			this.setState({deviceID: device_id});
+		})
+		this.setState({player_Spotify: player});
+	}
+
+	nextSong = async (e) => {
 		e.preventDefault();
 		let currentSong = this.state.currentPlaying;
 		const currentList = this.state.currentSongList;
@@ -49,7 +87,48 @@ class HostRoomContainer extends React.Component {
 			currentPlaying: currentSong,
 			currentSongList: currentList
 		});
+
+		console.log(this.state.currentSongList);
+		console.log(currentSong);
+
+		const response = await fetch('/profile', {
+			method: 'GET',
+			headers: { 'Content-Type': 'applications/json' }
+		});
+		const responseJSON = await response.json();
+		const token = responseJSON.currentAccessToken;
+		console.log(token);
+
+		let songRequest = {
+			method: 'PUT',
+			headers: {
+				'Authorization': 'Bearer ' + token
+			},
+			body: JSON.stringify({
+				"uris": [currentSong.trackURI],
+				"offset": {
+					"position": 0
+				},
+				"position_ms": 0
+			}),
+			mode: 'cors',
+			cache: 'default'
+		};
+
+		const playURL = "https://api.spotify.com/v1/me/player/play?device_id=" + this.state.deviceID; //+ this.state.deviceID;
+		await fetch(playURL, songRequest);
 	};
+
+	pauseSong = async (e) => {
+		if (e){
+			e.preventDefault();
+		}
+		console.log("trying to pause song");
+		this.state.player_Spotify.togglePlay().then(() => {
+			console.log('Toggled playback!');
+		});
+
+	}
 
 	acceptSong(song) {
 		//remove from suggestedList
@@ -127,7 +206,7 @@ class HostRoomContainer extends React.Component {
 					</div>
 				</div>
 
-				<CurrentlyPlaying getnextsong={this.nextSong} songList={this.state} premium="true" />
+				<CurrentlyPlaying getnextsong={this.nextSong} pause={this.pauseSong} songList={this.state} premium="true" />
 			</div>
 		);
 	}
